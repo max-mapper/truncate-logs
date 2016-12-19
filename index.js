1ea49b57f60ab22b57f9dd835e56b1191be30fab
Max Ogden <max@maxogden.com>
2016-12-19 11:51:35 -0800

    add glob

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var os = require('os')
 var path = require('path')
+var glob = require("glob")
 var pump = require('pump')
 var sf = require('slice-file')
 var series = require('run-series')
@@ -13,14 +14,26 @@ module.exports = function (files, opts, cb) {
     opts = {}
   }
   if (!opts) opts = {}
+  
+  if (files[0].indexOf('*') > -1) {
+    glob(files[0], function (err, globbed) {
+      if (err) return cb(err)
+      if (!globbed.length) return cb()
+      truncateAll(globbed, cb)
+    })
+  } else {
+    truncateAll(files, cb)
+  }
 
-  var items = files.map(function (f) {
-    return function (cb) {
-      truncate(f, cb)
-    }
-  })
+  function truncateAll (files, cb) {
+    var items = files.map(function (f) {
+      return function (cb) {
+        truncate(f, cb)
+      }
+    })
 
-  series(items, cb)
+    series(items, cb)
+  }
   
   function truncate (file, cb) {
     var xs = sf(file)
